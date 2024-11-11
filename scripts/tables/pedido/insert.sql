178d278a86419c7570a4c01ca290b8913414ad06
Uadson Feitosa <uadsonfeitosa@goiania.go.gov.br>
2024-11-11 15:57:31 -0300

    pedido_insert

---
+++ scripts/tables/pedido/insert.sql
@@ -0,0 +1,59 @@
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    1, '2008-04-01', 1300, 1, 1, 1
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    2, '2008-04-01', 500, 1, 1, 1
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    3, '2008-04-02', 300, 11, 2, 5
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    4, '2008-04-05', 1000, 11, 1, 7
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    5, '2008-04-06', 200, 9, 2, 6
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    6, '2008-04-06', 1985, 10, 1, 6
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    7, '2008-04-06', 800, 3, 1, 7
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idvendedor) values (
+    8, '2008-04-06', 175, 3, 7
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idvendedor) values (
+    9, '2008-04-07', 1300, 12, 8
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    10, '2008-04-10', 200, 6, 1, 8
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    11, '2008-04-15', 300, 15, 2, 1
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    12, '2008-04-20', 500, 15, 2, 5
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    13, '2008-04-20', 350, 9, 1, 7
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor) values (
+    14, '2008-04-23', 300, 2, 1, 5
+);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idvendedor) values (
+    15, '2008-04-25', 200, 11, 5
+);
